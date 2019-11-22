8961d84b2c34652caa25eadbc728e04ebf9c256a
KEONHEE <danmi765@naver.com>
2019-11-22 18:52:47 +0900

    레이아웃정리

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SideMenu from './SideMenu.js';
+import Main from './Main.js';
+
+
+class App extends React.Component{
+
+  constructor(props){
+      super(props);
+      this.state = {
+          main : <Main />
+      }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className="inner">
+          <SideMenu />
+          {this.state.main}
+        </div>
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
